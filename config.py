06a2c0a591f5b1c7fd744113a3e411229928e7a6
Riyuman <139713314+Riyuman@users.noreply.github.com>
2024-05-03 16:15:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,9 +12,9 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 # Add Owner Username without @ 
-OWNER_USERNAME = getenv("OWNER_USERNAME","expertcoderr")
+OWNER_USERNAME = getenv("OWNER_USERNAME","Oogo_nick")
 # Get Your bot username
-BOT_USERNAME = getenv("BOT_USERNAME" , "CuteshizuBot")
+BOT_USERNAME = getenv("BOT_USERNAME" , "MUSIC_M_P_G_Bot")
 # Don't Add style font 
 BOT_NAME = getenv("BOT_NAME" , "Shizu")
 #get Your Assistant User name
@@ -26,10 +26,10 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1001801976314))
+LOGGER_ID = int(getenv("LOGGER_ID", -1001899134283)
 
 # Get this value from  on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6481884068))
+OWNER_ID = int(getenv("OWNER_ID", 5637296836)
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -39,15 +39,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TMM-TEAM/Nobimusic",
+    "https://github.com/Riyuman/Nobimusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/tmm_heroku_world")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tmm_support_chat")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/movie_provider_official")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/movie_providing_group")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -121,7 +121,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/cfbdee8103102bcb2e5da.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/1b798dcc380d01f886d15.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/00360393a15daf7fc4e9d.jpg"
